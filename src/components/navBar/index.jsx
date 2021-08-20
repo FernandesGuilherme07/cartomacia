1dcdb63ea67c136cdbffe59fa5c7ba013343f441
Guilherme Fernandes <72757771+FernandesGuilherme07@users.noreply.github.com>
2021-08-19 21:37:14 -0300

    navbar responsive

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -1,19 +1,24 @@
 import { useState } from "react";
-import  { Buttons, Header, MenuToggle }  from "./navBar.styled"
+import  { Header, MenuToggle, NavBarToggle }  from "./navBar.styled"
 import { IoMdMenu } from "react-icons/io";
 
 export default function NavBar () {
 
-    const [show, setShow] = useState(false);
+    const [show, setShow] = useState(false)
 
-    const ToggleMenu = () => setShow(!show);
+    const ToggleMenu = () => {
+        setShow(!show);
+        
+    }
 
     return (
+        <>
+    
         <Header>
            <div>
                <img src="/logo.svg" alt="logo" />
            </div>
-           <div show= {show}>
+           <div >
                 <div>Início</div>
                 <div>Cartomancia</div>
                 <div>Historia na cartomancia</div>
@@ -21,12 +26,24 @@ export default function NavBar () {
                 <div>Preços</div>
             </div>
         
-            <MenuToggle onClick={ToggleMenu}>
-                <IoMdMenu/>
+            <MenuToggle >
+                <IoMdMenu onClick={ToggleMenu} />
             </MenuToggle>
+                
                 
             
 
         </Header>
+      
+        <NavBarToggle show={show}>
+    
+            <div>Início</div>
+            <div>Cartomancia</div>
+            <div>Historia na cartomancia</div>
+            <div>Baralhos</div>
+            <div>Preços</div>
+        </NavBarToggle>
+
+        </>
     )
 }
--- src/components/navBar/navBar.styled.jsx
+++ src/components/navBar/navBar.styled.jsx
@@ -9,7 +9,7 @@ export const Header =  styled.head`
     background: #351B3F;
     align-itens: center;
     position: fixed;
-    top: 0;
+    top: 0;     
 
     div {
         margin: 0 10% 0 10%;
@@ -39,6 +39,10 @@ export const Header =  styled.head`
 
             @media (max-width: 950px) {
                 display: none;
+                flex-direction: collum;
+                align-itens: center;
+                padding: 1rem;
+
                 }
   
             }
@@ -47,15 +51,50 @@ export const Header =  styled.head`
 
 export const MenuToggle = styled.div`
 
-    font-size: 55px;
+    font-size: 3.4rem;
     color: #ddc41c;
     position: relative;
     right: 5%;
     cursor: pointer;
 
     @media (min-width: 950px) {
-        display: ${({ show }) => show ? 'block' : 'none'} !important; 
+        display: none !important;
     }
     
     
 `;
+
+export const NavBarToggle = styled.div`
+    display: ${({ show }) => show ? 'flex' : 'none'} ; 
+    flex-direction: column;
+    align-items: center;
+    width: 70%;
+    height: 100%;
+    position: fixed;
+    right: 0;
+    top: 80px;
+    background: #351B3F;
+    z-index: 999;
+    border-top: 1px solid #0B0D17;
+
+    @media (min-width: 950px) {
+        display: none !important;
+    }
+    
+    div {
+        margin: 25px 5px;
+        padding: 8px 40px;
+        color: #ddc41c;
+        border-radius: 5px;
+        cursor: pointer;
+        
+        :hover{
+            background: #6c5077;
+        }
+    }
+     
+     
+   
+`;
+   //margin-top: 5px;
+    //padding: 5px;
